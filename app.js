590765aa565b62eb48d98dae8c1cd7edc3f8e72a
Tasha <xtasherx@gmail.com>
2020-09-29 21:43:12 -0500

    set up classes and got unit tests working

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
-class Charachter {
-    constructor(name,id,email){
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-    getName(){};
-    getID(){};
-    getEmail(){};
-    getRole(){};
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/engineer");
+
+function processAns(ans) {
+  console.log(ans);
 }
 
-class Manager extends Employee{
-    
-    constructor(officeNumber){
-        this.officeNumber = officeNumber;       
-    }
-    getRole();
-}
-
-class Engineer extends Employee{
-    constructor(github){
-        this.github = github;
-    }
-
-    getGithub(){};
-    getRole(){};
-
-}
-
-class Intern extends Employee{
-    constructor(school){
-        this.school = school;
-    }
-    getRole(){};
-}
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "empRole",
+      message: "What is this team member's role?",
+      choices: ["Manager", "Engineer", "Intern"],
+    },
+    {
+      message: "What is the team member's name?",
+      name: "empName",
+    },
+    {
+      message: "What is the email for this team member?",
+      name: "empEmail",
+    },
+    {
+      message: "What is the office number for the manager?",
+      name: "mgrNumber",
+      when: (ans) => ans.empRole === "Manager",
+    },
+    {
+      message: "What is the engineers github user name?",
+      name: "engGitUser",
+      when: (ans) => ans.empRole === "Engineer",
+    },
+    {
+      message: "What school is this intern with?",
+      name: "intSchool",
+      when: (ans) => ans.empRole === "Intern",
+    },
+  ])
+  .then(processAns);
