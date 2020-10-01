38ca278698fefe33da9f2a5e716cd0954eabb5c2
Tasha <xtasherx@gmail.com>
2020-09-30 22:03:59 -0500

    all prompts working--loop for adding members

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Engineer = require("./lib/engineer");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const render = require("./lib/htmlRenderer");
 
-function processAns(ans) {
-  console.log(ans);
-}
+const employees = [];
 
-inquirer
-  .prompt([
+const collectInput = async (input = []) => {
+  const questions = [
     {
       type: "list",
       name: "empRole",
       message: "What is this team member's role?",
       choices: ["Manager", "Engineer", "Intern"],
+    },
+    {
+      message: "What is the team member's ID #?",
+      name: "empID",
     },
     {
       message: "What is the team member's name?",
@@ -40,5 +45,52 @@ inquirer
       name: "intSchool",
       when: (ans) => ans.empRole === "Intern",
     },
-  ])
-  .then(processAns);
+    {
+      type: "confirm",
+      name: "again",
+      message: "Would you like to add a team member?",
+    },
+  ];
+
+  const { again, ...answers } = await inquirer.prompt(questions);
+  const newInput = [...input, answers];
+  return again ? collectInput(newInput) : newInput;
+};
+
+const main = async () => {
+  const input = await collectInput();
+  input.forEach((employee) => {
+    if (employee.empRole === "Manager") {
+      const mgr = new Manager(
+        employee.empName,
+        employee.empID,
+        employee.empEmail,
+        employee.mgrNumber
+      );
+      employees.push(mgr);
+    } else if (employee.empRole === "Engineer") {
+      const eng = new Engineer(
+        employee.empName,
+        employee.empID,
+        employee.empEmail,
+        employee.engGitUser
+      );
+      employees.push(eng);
+    } else {
+      const int = new Intern(
+        employee.empName,
+        employee.empID,
+        employee.empEmail,
+        employee.intSchool
+      );
+      employees.push(int);
+    }
+  });
+};
+
+main();
+
+function buildPage() {
+  fs.writeFileSync(outputPath, render(employees), "utf-8");
+}
+buildPage();
